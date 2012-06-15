5ee894ea3d54932e5f0c7a9cbd458bfce90a01fc
Thomas Risberg <trisberg@vmware.com>
2012-06-15 11:17:49 -0400

    Adding unit test for HelloOracle

--- jdbc/src/main/java/com/oreilly/springdata/jdbc/oracle/HelloOracle.java
+++ jdbc/src/main/java/com/oreilly/springdata/jdbc/oracle/HelloOracle.java
@@ -1,8 +1,7 @@
 package com.oreilly.springdata.jdbc.oracle;
 
-import org.springframework.dao.DataAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.support.DatabaseMetaDataCallback;
 import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.jdbc.support.MetaDataAccessException;
@@ -10,54 +9,38 @@ import org.springframework.jdbc.support.MetaDataAccessException;
 import javax.sql.DataSource;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HelloOracle {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Oracle");
-		HelloOracle app = new HelloOracle();
-		app.test();
+	DataSource dataSource;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
-	public void test() {
-
-		SimpleDriverDataSource ds = new SimpleDriverDataSource(new oracle.jdbc.OracleDriver(),"jdbc:oracle:thin:@redwood:1521:XE", "spring", "spring");
-
-		try {
-			printConnectionMetaData(ds);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		JdbcTemplate db = new JdbcTemplate(ds);
-
-		try {
-			List<Map<String, Object>> results = db.queryForList("select * from dual");
-			System.out.println(results);
-		} catch (DataAccessException e) {
-			if (e.getRootCause() instanceof SQLException) {
-				SQLException se = (SQLException) e.getRootCause();
-				System.out.println(se.getErrorCode() + " " + se.getSQLState() + " " + se.getMessage());
-			}
-			else {
-				System.out.println(e.getMessage());
-			}
-		}
+	public String getDualFromDataSource() {
+		JdbcTemplate db = new JdbcTemplate(dataSource);
+		String results = db.queryForObject("select dummy from dual", String.class);
+		return results;
 	}
 
-	private void printConnectionMetaData(DataSource dataSource) throws Exception {
-     JdbcUtils.extractDatabaseMetaData(dataSource,
-             new DatabaseMetaDataCallback() {
-                 public Object processMetaData(DatabaseMetaData databaseMetaData) throws SQLException, MetaDataAccessException {
-					 System.out.println("Database Name: " + databaseMetaData.getDatabaseProductName());
-                     System.out.println("Database Version: " + databaseMetaData.getDatabaseProductVersion());
-                     System.out.println("Driver Name: " + databaseMetaData.getDriverName());
-                     System.out.println("Driver Version: " + databaseMetaData.getDriverVersion());
-                     return null;
-                 }
-             });
+	public Map<String, String> getDatabaseMetaData() throws MetaDataAccessException {
+
+		final Map<String, String> metaData = new HashMap<String, String>();
+     	return (Map<String, String>) JdbcUtils.extractDatabaseMetaData(dataSource,
+				 new DatabaseMetaDataCallback() {
+					 public Object processMetaData(DatabaseMetaData databaseMetaData) throws SQLException, MetaDataAccessException {
+						 metaData.put("Database Name", databaseMetaData.getDatabaseProductName());
+						 metaData.put("Database Version", databaseMetaData.getDatabaseProductVersion());
+						 metaData.put("Driver Name", databaseMetaData.getDriverName());
+						 metaData.put("Driver Version", databaseMetaData.getDriverVersion());
+						 return metaData;
+					 }
+				 });
+
  }
 
 
