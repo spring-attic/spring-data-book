bbd6c430ed15d45e822be13a4071807169b55fbf
Thomas Risberg <trisberg@vmware.com>
2012-07-16 18:08:50 -0400

    cleanup SQL

--- jdbc/src/main/resources/sql/schema.sql
+++ jdbc/src/main/resources/sql/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE customer (
 CREATE UNIQUE INDEX ix_customer_email ON CUSTOMER (email_address ASC);
 CREATE TABLE address (
   id BIGINT IDENTITY PRIMARY KEY,
-  customer_id BIGINT CONSTRAINT address_customer_ref FOREIGN KEY REFERENCES customer (id),
+  customer_id BIGINT CONSTRAINT address_customer_ref REFERENCES customer (id),
   street VARCHAR(255),
   city VARCHAR(255),
   country VARCHAR(255));
