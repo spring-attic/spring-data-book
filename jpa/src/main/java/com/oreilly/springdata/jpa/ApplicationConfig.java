b68b25b6cbd43d8c7b2ce0b97eb1e332661e0290
Jon Brisbin <jon@jbrisbin.com>
2012-07-17 17:37:38 -0500

    More example code for Redis chapter covering PubSub and Caching.

--- jpa/src/main/java/com/oreilly/springdata/jpa/ApplicationConfig.java
+++ jpa/src/main/java/com/oreilly/springdata/jpa/ApplicationConfig.java
@@ -35,7 +35,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * Spring JavaConfig configuration class to setup a Spring container and infrastructure components like a
  * {@link DataSource}, a {@link EntityManagerFactory} and a {@link PlatformTransactionManager}.
- * 
+ *
  * @author Oliver Gierke
  */
 @Configuration
@@ -44,52 +44,50 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 class ApplicationConfig {
 
-	/**
-	 * Bootstraps an in-memory HSQL database.
-	 * 
-	 * @see http 
-	 *      ://static.springsource.org/spring/docs/3.1.x/spring-framework-reference/html/jdbc.html#jdbc-embedded-database
-	 *      -support
-	 * @return
-	 */
-	@Bean
-	public DataSource dataSource() {
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		return builder.setType(EmbeddedDatabaseType.HSQL).build();
-	}
+  /**
+   * Bootstraps an in-memory HSQL database.
+   *
+   * @return
+   * @see http://static.springsource.org/spring/docs/3.1.x/spring-framework-reference/html/jdbc.html#jdbc-embedded-database-support
+   */
+  @Bean
+  public DataSource dataSource() {
+    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+    return builder.setType( EmbeddedDatabaseType.HSQL ).build();
+  }
 
-	/**
-	 * Sets up a {@link LocalContainerEntityManagerFactoryBean} to use Hibernate. Activates picking up entities from the
-	 * project's base package.
-	 * 
-	 * @return
-	 */
-	@Bean
-	public EntityManagerFactory entityManagerFactory() {
+  /**
+   * Sets up a {@link LocalContainerEntityManagerFactoryBean} to use Hibernate. Activates picking up entities from the
+   * project's base package.
+   *
+   * @return
+   */
+  @Bean
+  public EntityManagerFactory entityManagerFactory() {
 
-		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		vendorAdapter.setGenerateDdl(true);
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setGenerateDdl( true );
 
-		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		factory.setJpaVendorAdapter(vendorAdapter);
-		factory.setPackagesToScan(getClass().getPackage().getName());
-		factory.setDataSource(dataSource());
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setJpaVendorAdapter( vendorAdapter );
+    factory.setPackagesToScan( getClass().getPackage().getName() );
+    factory.setDataSource( dataSource() );
 
-		factory.afterPropertiesSet();
+    factory.afterPropertiesSet();
 
-		return factory.getObject();
-	}
+    return factory.getObject();
+  }
 
-	@Bean
-	public JpaDialect jpaDialect() {
-		return new HibernateJpaDialect();
-	}
+  @Bean
+  public JpaDialect jpaDialect() {
+    return new HibernateJpaDialect();
+  }
 
-	@Bean
-	public PlatformTransactionManager transactionManager() {
+  @Bean
+  public PlatformTransactionManager transactionManager() {
 
-		JpaTransactionManager txManager = new JpaTransactionManager();
-		txManager.setEntityManagerFactory(entityManagerFactory());
-		return txManager;
-	}
+    JpaTransactionManager txManager = new JpaTransactionManager();
+    txManager.setEntityManagerFactory( entityManagerFactory() );
+    return txManager;
+  }
 }
