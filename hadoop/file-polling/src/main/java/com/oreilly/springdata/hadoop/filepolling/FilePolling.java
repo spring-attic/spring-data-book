df9529d5cba3125de723e7083daff4f4ab95180c
mpollack <mpollack@vmware.com>
2012-08-04 21:58:43 -0400

    Add example copying local file to HDFS using SI inbound file adapter

---
+++ hadoop/file-polling/src/main/java/com/oreilly/springdata/hadoop/filepolling/FilePolling.java
@@ -0,0 +1,19 @@
+package com.oreilly.springdata.hadoop.filepolling;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class FilePolling {
+
+	private static final Log log = LogFactory.getLog(FilePolling.class);
+
+	public static void main(String[] args) throws Exception {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"/META-INF/spring/application-context.xml", FilePolling.class);
+		log.info("File Polling Application Running");
+		context.registerShutdownHook();
+
+	}
+}
