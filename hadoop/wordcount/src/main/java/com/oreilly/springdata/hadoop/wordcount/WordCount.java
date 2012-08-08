28717dc3894e274f1ccfc2739b12d3eba9896e0b
mpollack <mpollack@vmware.com>
2012-08-08 15:47:03 -0400

    clean up imports

--- hadoop/wordcount/src/main/java/com/oreilly/springdata/hadoop/wordcount/WordCount.java
+++ hadoop/wordcount/src/main/java/com/oreilly/springdata/hadoop/wordcount/WordCount.java
@@ -12,7 +12,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
 
