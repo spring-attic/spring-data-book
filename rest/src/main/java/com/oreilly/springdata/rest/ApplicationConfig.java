0e5aef0c5570dad83501f3e1b98c48e48072938d
Oliver Gierke <info@olivergierke.de>
2012-08-23 12:45:59 +0200

    REST - Improve application configuration.

--- rest/src/main/java/com/oreilly/springdata/rest/ApplicationConfig.java
+++ rest/src/main/java/com/oreilly/springdata/rest/ApplicationConfig.java
@@ -24,11 +24,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaDialect;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -66,7 +64,7 @@ class ApplicationConfig {
 	 * @return
 	 */
 	@Bean
-	public EntityManagerFactory entityManagerFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setDatabase(Database.HSQL);
@@ -77,21 +75,14 @@ class ApplicationConfig {
 		factory.setPackagesToScan(getClass().getPackage().getName());
 		factory.setDataSource(dataSource());
 
-		factory.afterPropertiesSet();
-
-		return factory.getObject();
-	}
-
-	@Bean
-	public JpaDialect jpaDialect() {
-		return new HibernateJpaDialect();
+		return factory;
 	}
 
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 
 		JpaTransactionManager txManager = new JpaTransactionManager();
-		txManager.setEntityManagerFactory(entityManagerFactory());
+		txManager.setEntityManagerFactory(entityManagerFactory().getObject());
 		return txManager;
 	}
 }
