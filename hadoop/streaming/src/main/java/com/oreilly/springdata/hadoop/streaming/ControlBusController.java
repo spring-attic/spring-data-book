da84e8cb7fb5860473cd6be0b46a6992b3672ceb
mpollack <mpollack@vmware.com>
2012-09-12 12:44:44 -0400

    update control-bus example to use groovy

--- hadoop/streaming/src/main/java/com/oreilly/springdata/hadoop/streaming/ControlBusController.java
+++ hadoop/streaming/src/main/java/com/oreilly/springdata/hadoop/streaming/ControlBusController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.integration.core.PollableChannel;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,14 +15,14 @@ public class ControlBusController {
 	
 	@Autowired
 	private MessageChannel inOperationChannel;	
-	@Autowired
-	private PollableChannel outOperationChannel;
 	
 	@RequestMapping("/admin")
 	public @ResponseBody String simple(@RequestBody String message) {
-		Message<String> operation = MessageBuilder.withPayload("@" + message).build();
-		inOperationChannel.send(operation);
-		Message response = outOperationChannel.receive(1000); 
+		
+		Message<String> operation = MessageBuilder.withPayload(message).build();
+		MessagingTemplate template = new MessagingTemplate();
+		Message response = template.sendAndReceive(inOperationChannel, operation);
+
 		if (response != null) {
 			return response.getPayload().toString();
 		} else {
