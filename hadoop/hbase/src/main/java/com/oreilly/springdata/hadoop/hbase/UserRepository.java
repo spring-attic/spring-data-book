c2fce1afb7ee1f3d5c0c28dd1f2e5498f26126f0
mpollack <mpollack@vmware.com>
2012-12-13 05:55:08 +0800

    update to latest callback signature

--- hadoop/hbase/src/main/java/com/oreilly/springdata/hadoop/hbase/UserRepository.java
+++ hadoop/hbase/src/main/java/com/oreilly/springdata/hadoop/hbase/UserRepository.java
@@ -3,6 +3,7 @@ package com.oreilly.springdata.hadoop.hbase;
 import java.util.List;
 
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -41,7 +42,7 @@ public class UserRepository {
 	public User save(final String userName, final String email,
 			final String password) {
 		return hbaseTemplate.execute(tableName, new TableCallback<User>() {
-			public User doInTable(HTable table) throws Throwable {
+			public User doInTable(HTableInterface table) throws Throwable {
 				User user = new User(userName, email, password);
 				Put p = new Put(Bytes.toBytes(user.getName()));
 				p.add(CF_INFO, qUser, Bytes.toBytes(user.getName()));
